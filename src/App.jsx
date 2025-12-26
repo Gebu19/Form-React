13230b73d8805b5a14868d5c31316a9a3e0d536e
Gebu19 <gebu830@gmail.com>
2025-12-26 16:06:02 +0700

    adede...

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Routes >
         
         <Route path='/' element={<Register/>}/>
+        <Route path='/deployFormRefReact/' element={<Register/>}/>
       
       </Routes>
     </>
